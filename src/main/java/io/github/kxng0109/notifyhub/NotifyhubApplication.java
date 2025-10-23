0a5d4b1b088cb19898aa76be35ab51047be9b656
Joshua Ike <79171733+kxng0109@users.noreply.github.com>
2025-10-23 11:06:04 +0100

    feat: Add OpenAPI documentation and dependency for NotifyHub API
    - Integrates `springdoc-openapi-starter-webmvc-ui` dependency in `pom.xml` for OpenAPI support.
    - Annotates `NotifyhubApplication` with `@OpenAPIDefinition` to document API details, including title, version, description, and contact information.

--- src/main/java/io/github/kxng0109/notifyhub/NotifyhubApplication.java
+++ src/main/java/io/github/kxng0109/notifyhub/NotifyhubApplication.java
@@ -1,13 +1,26 @@
 package io.github.kxng0109.notifyhub;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "NotifyHub API",
+                description = "An asynchronous microservice for queuing and dispatching notifications (e.g., email) via RabbitMQ.",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Joshua"
+                )
+        )
+)
 @SpringBootApplication
 public class NotifyhubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotifyhubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyhubApplication.class, args);
+    }
 
 }
